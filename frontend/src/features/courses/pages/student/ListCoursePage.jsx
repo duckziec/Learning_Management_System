456357381395af56cbc2833650dd8b451eb9c880
Phuong Nguyen <nguyenhoaiphuong010105@gmail.com>
2026-05-29 17:17:09 +0700

    feat(fe): Add LearningAnalytics and ListCoursePage components with associated styles for course analytics and listing

---
+++ frontend/src/features/courses/pages/student/ListCoursePage.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import AnimatedPage from "../../../../components/ui/AnimatedPage";
+import ListCourseContent from "../../components/student/ListCourse/ListCourseContent";
+
+export default function ListCoursePage() {
+    return (
+        <AnimatedPage>
+            <ListCourseContent />
+        </AnimatedPage>
+    );
+}
